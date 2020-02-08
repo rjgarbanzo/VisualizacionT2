e25aa867d40496eb6d7578529e539fa16b8800a4
rjgarbanzo <rjgarbanzo@gmail.com>
2020-02-08 00:04:14 -0600

    Inicio ejercicios 2

--- Script.R
+++ Script.R
@@ -15,3 +15,38 @@ g
 
 
 
+#########################################################################
+
+library(dplyr)
+library(sf)
+library(scales)
+
+cantones <- read_sf("geo_data/geo_data/cantones/cantones.shp")
+
+IDH <- IDH_cantonal_2014
+to_upper <- function(x) toupper(chartr("áéíóúÁÉÍÓÚ", "aeiouAEIOU", x))
+IDH$Cantón <- to_upper(IDH$Cantón)
+names(IDH)[1] <- "Posicion"
+names(IDH)[2] <- "Canton"
+IDH <- IDH[-1]
+
+
+cantones <- left_join(x = cantones, y = IDH, by = c("cant_nom1" = "Canton"))
+
+cantones <- subset(cantones, cantones$prov_nom1 == "SAN JOSE")
+
+
+ggplot(cantones, aes(fill = cantones$IDH)) +
+  geom_sf(color = "black", size = .1) +
+  theme_void() +
+  scale_fill_distiller(labels = comma,
+                       trans = "log10",
+                       name = "Cantidad de delitos reportados",
+                       direction = 1) +
+  labs(title = "Distribución de los delitos reportados en Costa Rica",
+       caption = "Fuente: Organismos De Investigación Judicial") +
+  theme(legend.key.height = unit(1,"cm"),
+        plot.title = element_text(hjust = .5, size = 12))
+
+
+
